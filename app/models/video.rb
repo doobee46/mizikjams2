a4a0c176fc3bfa9af053944059b0be1619f8b373
obedlorisson <obed.lorisson@gmail.com>
2015-08-01 14:25:58 +0000

    Env variable fix

--- app/models/video.rb
+++ app/models/video.rb
@@ -8,12 +8,14 @@ class Video < ActiveRecord::Base
     
     is_impressionable :column_name => :views, :unique => :session_hash, :counter_cache => true
     
-    if Rails.env.development?
-    has_attached_file :image, :styles => { :large=> "1214x450#",:medium => "288x150#", :thumb => "100x100>", :avatar =>"64x64#", :square =>"578x315#" }, :default_url => "default_:style.png"
-    else
+    if Rails.env.production?
     has_attached_file :image, :styles => { :large=> "1214x450#",:medium => "288x150#", :thumb => "100x100>", :avatar =>"64x64>", :square =>"578x315#" }, :default_url => "default_:style.png",
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    else
+    has_attached_file :image, :styles => { :large=> "1214x450#",:medium => "288x150#", :thumb => "100x100>", :avatar =>"64x64#", :square =>"578x315#" }, :default_url => "default_:style.png"    
+    
+    
     end
     validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png","image/jpg"] }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
