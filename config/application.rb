d6c3ae66b543e21e413373f94a9617edb9de2115
obedlorisson <obed.lorisson@gmail.com>
2015-08-23 22:40:46 +0000

    static asset fix

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Mizikjams
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.font_assets.origin = '*'
+    config.font_assets.origin = 'http://mizikjams.com'
   end
 end
